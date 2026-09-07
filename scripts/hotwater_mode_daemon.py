a7ce05ee0278aeeccc261de08ba22efd15a6f80c
Iain Bate <ijb500@uoy26m288.tail7b6124.ts.net>
2026-09-07 23:53:56 +0100

    chore: auto-commit after tool use at 2026-09-07 23:53:56

--- scripts/hotwater_mode_daemon.py
+++ scripts/hotwater_mode_daemon.py
@@ -42,6 +42,8 @@ from hotwater_automation_core import (
     check_legionella_due_warning,
     get_config_path,
     get_hotwater_automation_config_error,
+    is_service_mode_active,
+    read_state,
     run_force_heat_check,
     run_legionella_natural_completion_check,
     run_legionella_progress_check,
